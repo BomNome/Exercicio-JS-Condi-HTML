e01c91d18568a165e90fe867b6616ffc5c4d2bc4
Vitor <132232270+BomNome@users.noreply.github.com>
2023-06-03 17:38:34 -0300

    Add files via upload

--- Exercicio/length.js
+++ Exercicio/length.js
@@ -1,8 +1,8 @@
 src='index.js'
 
 if((nome.length)<=6){
-    document.body.innerHTML += `<h2>O Nome tem um tamanho razoavel, de 6 ou menos caracteres.</h2>`
+    document.body.innerHTML += `<h2 style='display: flex; justify-content: center;'>O Nome tem um tamanho razoavel, de 6 ou menos caracteres.</h2>`
 }
 else{
-    document.body.innerHTML += `<h2>O nome tem um tamanho SUPERIOR a de 6 caracteres.</h2>`
+    document.body.innerHTML += `<h2 style='display: flex; justify-content: center;'>O nome tem um tamanho SUPERIOR a de 6 caracteres.</h2>`
 }
