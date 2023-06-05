1d6b7e244099cd091eb50d173f946d066f77fe24
Vitor <132232270+BomNome@users.noreply.github.com>
2023-06-05 17:05:27 -0300

    Add files via upload

--- Exercicio/index.js
+++ Exercicio/index.js
@@ -1,6 +1,6 @@
 const nome = prompt('Digite um nome qualquer.');
     
-    document.body.innerHTML = `<h1 style='display: flex; justify-content: center;'>Nome digitado: ${nome}</h1><br />`;
+    document.body.innerHTML = `<h1 class="scr">Nome digitado: ${nome}</h1><br />`;
     
 
 
